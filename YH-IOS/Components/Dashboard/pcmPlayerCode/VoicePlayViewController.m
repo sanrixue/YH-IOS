5400ea63eb257dfc567fbf517aae32b7c61c6fb8
qiandashuai <1055263815@qq.com>
2016-12-01 17:50:13 +0800

    add:bug-outlink-error

--- YH-IOS/Components/Dashboard/pcmPlayerCode/VoicePlayViewController.m
+++ YH-IOS/Components/Dashboard/pcmPlayerCode/VoicePlayViewController.m
@@ -12,7 +12,6 @@
 #import "iflyMSC/IFlySpeechUtility.h"
 #import "iflyMSC/IFlySpeechConstant.h"
 #import <AVFoundation/AVFoundation.h>
-#import "VoiceSpeechView.h"
 #import "PcmPlayer.h"
 #import "PcmPlayerDelegate.h"
 #import "FileUtils.h"
@@ -72,7 +71,7 @@
 }
 
 - (void)viewDidAppear:(BOOL)animated {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [self voiceSppech];
     });
 }

365f8c3d0a47c5b2c4407ca2bcb95dccee3d7ccb
qiandashuai <1055263815@qq.com>
2017-01-13 10:40:08 +0800

    add:

---
+++ YH-IOS/Components/Login/SendResetPasswordViewController.h
@@ -0,0 +1,13 @@
+//
+//  SendResetPasswordViewController.h
+//  YH-IOS
+//
+//  Created by li hao on 17/1/12.
+//  Copyright © 2017年 com.intfocus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SendResetPasswordViewController : UIViewController
+
+@end
---
+++ YH-IOS/Components/Login/SendResetPasswordViewController.m
@@ -0,0 +1,37 @@
+//
+//  SendResetPasswordViewController.m
+//  YH-IOS
+//
+//  Created by li hao on 17/1/12.
+//  Copyright © 2017年 com.intfocus. All rights reserved.
+//
+
+#import "SendResetPasswordViewController.h"
+
+@interface SendResetPasswordViewController ()
+
+@end
+
+@implementation SendResetPasswordViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
